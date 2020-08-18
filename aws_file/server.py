1ac963b67e46d14510c9af2c1d8da3dcd079ace2
nano-2-ly <dong991207@unist.ac.kr>
2020-08-18 19:44:06 +0900

    update

--- aws_file/server.py
+++ aws_file/server.py
@@ -1,33 +1,40 @@
 import socket
-
+  
 print("program start!")
 
-HOST = ''                 # Symbolic name meaning all available interfaces
-PORT = 8080           # Arbitrary non-privileged port
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('', 8080))
-s.listen(1)
+HOST = ''                 
+PORT = 8080           
+python_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+python_client.bind(('', 8080))
+python_client.listen(1)
 
-p = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-p.bind(('', 8008))
-p.listen(1)
-
-
-conn1, addr1 = s.accept()
+nodemcu_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+nodemcu_client.bind(('', 8008))
+nodemcu_client.listen(1)
+          
+         
+conn1, addr1 = python_client.accept()
 print("python accepted!")
-
-conn2, addr2 = p.accept()
+        
+conn2, addr2 = nodemcu_client.accept()
 print("nodeMCU accepted!")
-
+      
 while True:
     try:
         data = conn1.recv(65535).decode()
         print(data)
+    except :
+        print("python connection has problem.")
+        conn1, addr1 = python_client.accept()
+        
+        data = conn1.recv(65535).decode()
+        print(data)
 
+    try:
+        conn2.sendall(data.encode())
+    except:
+        print("nodeMCU connection has problem.")
+        conn2, addr2 = nodemcu_client.accept()
+        
         conn2.sendall(data.encode())
 
-    except :
-        print("exception occur")
-        conn1, addr1 = s.accept()
-        conn2, addr2 = p.accept()
-        print("accepted!")
